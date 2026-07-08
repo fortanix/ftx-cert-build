b71bda315d4190272114b4c9227fb04fa45633be
pavankalyan0424 <pavan.addepalli@fortanix.com>
2026-07-08 22:31:48 +0530

    [RTE-1077] Add license headers and update toml to use crate dependencies

--- src/crypto_provider.rs
+++ src/crypto_provider.rs
@@ -1,3 +1,8 @@
+/* Copyright (c) Fortanix, Inc.
+|*
+|* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+|* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 //! File containing different traits and definitions that need to be instantiated by a specific crypto provider in order too use this library
 
 use mbedtls::rng::Random;
--- src/mbedtls_adapter.rs
+++ src/mbedtls_adapter.rs
@@ -1,3 +1,8 @@
+/* Copyright (c) Fortanix, Inc.
+|*
+|* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+|* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use mbedtls::hash::{self, Type as HashType};
 use mbedtls::pk::{Pk, Type as PkType, ECDSA_MAX_LEN};
 use mbedtls::rng::{Random, Rdrand};
